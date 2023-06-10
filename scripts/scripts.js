1d6dcb46dafbe0ae134df612d1cb5fe1d7c60661
Yami <134548564+YamiKampfer@users.noreply.github.com>
2023-06-10 00:40:10 -0300

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,155 @@
+
+alert("hola");
+
+//Punto 1
+
+let nombre = "Yami";
+console.log(nombre);
+
+
+
+ //Punto 2
+
+let edad = 28;
+alert("Tengo " + edad + " años");
+
+
+
+//Punto 3
+
+let num1 = 6;
+let num2 = 3;
+let suma = num1 + num2;
+console.log(suma);
+
+
+
+//Punto 4
+
+let frase = "Hola Yami Bienvenida :)";
+console.log(frase.length);
+
+
+/*
+
+if(condicion){
+    //codigo si se cumple
+}
+
+else{
+    //codigo si no se cumple
+};
+
+
+if(condicion){
+    //codigo si se cumple
+
+}else if(condicionDos){
+    //codigo si se cumple otra condicion
+
+}else{
+    //codigo si no se cumple
+};
+
+*/
+
+
+/*
+switch (condicion){
+    case valor1:
+        //codigo para valor1
+        break;
+
+        case valor2:
+            //codigo para valor2
+            break;
+
+            case valor3:
+        //codigo para valor3
+        break;
+
+        case valor4:
+        //codigo para valor4
+        break;
+
+            default:
+                //codigo por defecto
+
+}
+
+*/
+
+
+let altura = 190;
+if(altura>=100){
+    alert( altura  + " Es mayor que 100")
+}else{
+    alert("Ingrese una altura mayor a 100")
+}
+
+/*
+
+switch (dia){
+case 1:
+    alert("lunes");
+    break;
+
+    case 2:
+        alert("martes");
+        break;
+
+        case 3:
+            alert("miercoles");
+            break;
+
+            case 4:
+                alert("jueves");
+                break;
+
+                case 5:
+                    alert("viernes");
+                    break;
+
+                    case 6:
+                        alert("sabado");
+                        break;
+
+                        case 7:
+                            alert("domingo");
+                            break;
+
+                            default:
+                                alert("numero incorrecto")
+}
+
+*/
+
+//Tarea if punto 1
+
+let edadUsuario = 25;
+if(edadUsuario>=25){
+    alert("es mayor que 25");
+}else{
+    alert("es menor que 25");
+}
+
+//Punto 2
+
+let numero = 5;
+if(numero>0){
+    console.log("numero es positivo");
+}else{(numero<0);
+console.log("numero es negativo");
+}
+
+
+//Punto 3
+
+
+
+
+
+
+
+
+
